bce024522cbcfa6b8a326be1e8830fd868ccb8c2
OvrK12 <krauso@tcd.ie>
2026-05-06 01:49:30 +0200

    Fixed imports in task registry

--- src/lengthgen/tasks/registry.py
+++ src/lengthgen/tasks/registry.py
@@ -2,30 +2,16 @@ from enum import Enum
 from dataclasses import dataclass, field
 from typing import Callable
 
-from .implementations.addition import generate_addition_dataset
 from .implementations.boolean_eval import generate_boolean_dataset
-from .implementations.multiplication import generate_multiplication_dataset
 from .implementations.parity import generate_parity_dataset
-from .implementations.permutation import generate_permutation_dataset
-from .implementations.permutation_new import generate_permutation_new_dataset
 from .implementations.permutation_binary import generate_permutation_binary_dataset
-from .implementations.permutation_prompting import generate_permutation_prompting_dataset
-from .implementations.variable_tracking import generate_variable_tracking_dataset
-from .implementations.position_tracking import generate_position_tracking_dataset
 
 class TaskType(str, Enum):
     PERMUTATION = "permutation"
-    PERMUTATION_NEW = "permutation_new"
     PERMUTATION_PROMPTING = "permutation_prompting"
     PERMUTATION_BINARY = "permutation_binary"
-    ADDITION = "addition"
-    ADDITION_NEW = "addition_new"
-    MULTIPLICATION = "multiplication"
     BOOLEAN = "boolean"
     PARITY = "parity"
-    VARIABLE = "variable_tracking"
-    POSITION = "position_tracking"
-
 
 @dataclass
 class TaskConfig:
@@ -58,25 +44,7 @@ TASK_CONFIGS = {
             random_start_index=False,
         )
     ),
-    TaskType.PERMUTATION: TaskConfig(
-        generator=generate_permutation_dataset,
-        base_params=dict(
-            num_objects=5,
-            include_indices=False,
-            delta_cot=False,
-        ),
-        train_params=dict(
-            random_start_index=True,
-            repetitive_mix=True,
-            repetitive_mix_ratio=0.2,
-            shuffle_operations=True,
-        ),
-        val_params=dict(
-            random_start_index=False,
-            shuffle_operations=False,
-            eval=True,
-        ),
-    ),
+
     TaskType.PERMUTATION_BINARY: TaskConfig(
         generator=generate_permutation_binary_dataset,
         base_params=dict(
@@ -96,6 +64,27 @@ TASK_CONFIGS = {
     ),
 }
 
+'''
+    TaskType.PERMUTATION: TaskConfig(
+        generator=generate_permutation_dataset,
+        base_params=dict(
+            num_objects=5,
+            include_indices=False,
+            delta_cot=False,
+        ),
+        train_params=dict(
+            random_start_index=True,
+            repetitive_mix=True,
+            repetitive_mix_ratio=0.2,
+            shuffle_operations=True,
+        ),
+        val_params=dict(
+            random_start_index=False,
+            shuffle_operations=False,
+            eval=True,
+        ),
+    ),
+'''
 def get_task_generator(task_name):
     if isinstance(task_name, str):
         try:
